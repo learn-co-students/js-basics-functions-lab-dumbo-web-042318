ea74da229e149ed8ea0211419545c27ad0d6181d
ahjuice <ahjuice@users.noreply.github.com>
2018-06-10 22:43:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+  const hq = 42;
+
+  return Math.abs(pickup - hq);
+}
+
+function distanceFromHqInFeet(pickup) {
+  return distanceFromHqInBlocks(pickup) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  blocks = Math.abs(destination - start);
+  return blocks * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  distance = distanceTravelledInFeet(start, destination);
+
+  switch (true) {
+    case distance <= 400:
+      return 0;
+    case distance > 400 && distance <= 2000:
+      return (distance - 400) * 0.02;
+    case distance > 2000 && distance <= 2500:
+      return 25;
+    case distance > 2500:
+      return "cannot travel that far";
+  }
+}
